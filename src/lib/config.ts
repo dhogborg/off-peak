412d3591c7d2460b52e11b06ddbe0207b66c3866
David Högborg <d@hogborg.se>
2022-08-30 16:46:56 +0200

    Beta mode feature flag

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,19 +4,25 @@ import { RootState } from './store'
 export type PeriodTypes = 'monthly' | 'continuous'
 export interface State {
   periodType: PeriodTypes
+  beta: boolean
 }
 
 const initialState: State = {
   periodType: 'continuous',
+  beta: false,
 }
 
 export const slice = createSlice({
-  name: 'tibber',
+  name: 'config',
   initialState,
 
   reducers: {
     setPeriod: (state, action: PayloadAction<PeriodTypes>) => {
       state.periodType = action.payload
+    },
+    setBetaMode: (state, action: PayloadAction<boolean>) => {
+      console.log(`We're in beta mode baby!`)
+      state.beta = action.payload
     },
   },
 })
@@ -24,4 +30,4 @@ export const slice = createSlice({
 export const selector = (state: RootState) => state.config
 
 export default slice.reducer
-export const { setPeriod } = slice.actions
+export const { setPeriod, setBetaMode } = slice.actions
