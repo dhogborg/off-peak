2ac8b5e7d90e0597e31c6eac0b9df9d23fe7cdc7
Erik Thorsell <erik@thorsell.io>
2023-06-13 06:57:12 +0200

    Add ability to view yearly consumption
    The following three views are added:
    
    * Last Year: Show consumption for the previous year (YYYY-01-01 to YYYY-12-31)
    * This Year: Show consumption from the beginning of this year to today
    * Running Year: Show consumption for the past 365 days

--- src/app/Graph/GraphLoader.lib.ts
+++ src/app/Graph/GraphLoader.lib.ts
@@ -25,3 +25,25 @@ export function getMonthIntervalFor(month: number): Period {
 
   return { from, to, hours }
 }
+
+export function getYearIntervalFor(year: number): Period {
+  const now = new Date()
+  now.setHours(0)
+  now.setMinutes(0)
+  now.setSeconds(0)
+  now.setMilliseconds(0)
+
+  const from = new Date(year, 0, 1)
+
+  let to: Date;
+  if (year < now.getFullYear()) {
+    to = new Date(year+1, 12, 31)  // if year is last year, stop on New Year's eve
+  } else {
+    to = now  // if year is this year, stop today
+  }
+
+  const ms = to.getTime() - from.getTime()
+  const hours = Math.floor(ms / 1000 / 60 / 60)
+
+  return { from, to, hours }
+}
